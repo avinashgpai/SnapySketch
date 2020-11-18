bab89c1ff3228625d33a05fc5c212e0bdc074e11
avinashgpai <avinashgpai98@gmail.com>
2020-11-18 23:26:51 +0530

    Updates

---
+++ precache-manifest.7608390a8a430af11a6d1922e32bd620.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a30a2667f75b62e5476e089980c51884",
+    "url": "/SnapySketch/index.html"
+  },
+  {
+    "revision": "dd3ec4946cafecb33081",
+    "url": "/SnapySketch/static/css/main.99f91cbd.chunk.css"
+  },
+  {
+    "revision": "4ce5db092750aa594df6",
+    "url": "/SnapySketch/static/js/2.bece80eb.chunk.js"
+  },
+  {
+    "revision": "6e900953f82e75bb7ccced89e6806918",
+    "url": "/SnapySketch/static/js/2.bece80eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd3ec4946cafecb33081",
+    "url": "/SnapySketch/static/js/main.a1d09086.chunk.js"
+  },
+  {
+    "revision": "48f51e791a987e22ad12",
+    "url": "/SnapySketch/static/js/runtime-main.611823e9.js"
+  },
+  {
+    "revision": "730534ffabb03ec0aba5e113033023ac",
+    "url": "/SnapySketch/static/media/download.730534ff.png"
+  },
+  {
+    "revision": "5f1e3a6f3f8a5ad6e405d81cc0e28687",
+    "url": "/SnapySketch/static/media/easy.5f1e3a6f.png"
+  },
+  {
+    "revision": "b8e4e0492d55507c75891a875442bda6",
+    "url": "/SnapySketch/static/media/facebook.b8e4e049.jpg"
+  },
+  {
+    "revision": "2e66725cbc67ceae8c1fb68ef11ba72c",
+    "url": "/SnapySketch/static/media/free_shadow.2e66725c.png"
+  },
+  {
+    "revision": "5a9c71cdfd1f2beeff46a721b3e4114a",
+    "url": "/SnapySketch/static/media/instagram.5a9c71cd.jpg"
+  },
+  {
+    "revision": "e7856ea81bd1cecb68db80b936ceb18f",
+    "url": "/SnapySketch/static/media/linkedin.e7856ea8.png"
+  },
+  {
+    "revision": "5d518075257130edd554d5aff1cdbec3",
+    "url": "/SnapySketch/static/media/move.5d518075.png"
+  },
+  {
+    "revision": "80fa15aa465ca72bbefd89612d2dfd36",
+    "url": "/SnapySketch/static/media/not-satisfied.80fa15aa.png"
+  },
+  {
+    "revision": "4950420bac3f8ede6dc496dab8f364df",
+    "url": "/SnapySketch/static/media/not-satisfied_onclick.4950420b.png"
+  },
+  {
+    "revision": "32d53fea70e5aeaaaecddaedf56582d9",
+    "url": "/SnapySketch/static/media/satisfied.32d53fea.png"
+  },
+  {
+    "revision": "b66b1e5e485a8f8e2ad846248638c0bf",
+    "url": "/SnapySketch/static/media/satisfied_onclick.b66b1e5e.png"
+  },
+  {
+    "revision": "402027305f4cdd474905384cbe7e217c",
+    "url": "/SnapySketch/static/media/twitter.40202730.jpg"
+  },
+  {
+    "revision": "22982bbeeafc3b6402518979c4aed374",
+    "url": "/SnapySketch/static/media/whatever.22982bbe.png"
+  },
+  {
+    "revision": "f0edf5a078320c77404b40601a38b266",
+    "url": "/SnapySketch/static/media/whatever_onclick.f0edf5a0.png"
+  }
+]);
